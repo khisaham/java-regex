07fa67ea9b59c8cf57470f8a7b28b0065c4badfb
khisaham <khisaham@gmail.com>
2019-08-27 21:59:18 +0300

    Add functions for regex patterns

--- src/main/java/com/khisaham/ke/regEx/MetaCharacter.java
+++ src/main/java/com/khisaham/ke/regEx/MetaCharacter.java
@@ -1,4 +1,18 @@
 package com.khisaham.ke.regEx;
 
 public class MetaCharacter {
+private static String pattern;
+    public static String removeWhiteSpaces(String inputText){
+        // Removes whitespace between a word character and . or ,
+        pattern = "(\\w)(\\s+)([\\.,])";
+        return inputText.replaceAll(pattern, "$1$3");
+    }
+
+    public static String extractTextBetweenAnElement(String inputText, String element){
+        // Extract the text between the two title elements
+        pattern = "(?i)(<"+element+".*?>)(.+?)()";
+        String updated = inputText.replaceAll(pattern, "$2");
+
+        return updated;
+    }
 }
--- src/main/java/com/khisaham/ke/regEx/NegativeLookAhead.java
+++ src/main/java/com/khisaham/ke/regEx/NegativeLookAhead.java
@@ -1,4 +1,7 @@
 package com.khisaham.ke.regEx;
 
 public class NegativeLookAhead {
+    //Negative look ahead provides the possibility to exclude a pattern. With this you can say that a string should not be followed by another string.
+    //Negative look ahead are defined via (?!pattern). For example, the following will match "a" if "a" is not followed by "b"
+    private static String negativeLookAheadPattern = "a(?!b)";
 }
